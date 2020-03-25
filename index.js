1806192cabff512cba8ee5e91d11d06ccccd002b
Stephen Cunningham <stephencork19@gmail.com>
2020-03-25 16:27:33 +0000

    server side and dialogflow connected v7

--- index.js
+++ index.js
@@ -9,4 +9,5 @@ app.use(bodyParse.json());
 require('./routes/dialogFlowRoutes')(app);
 
 const PORT = process.env.PORT || 5000;//PORT has a dynamic variable read from the environment variable on Heroku and value 5000 locally
+console.log('Port is: ' + PORT);
 app.listen(PORT);//this tells the app to listen on the define port number
