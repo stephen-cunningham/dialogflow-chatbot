3682493bd8a8627a76d717ccab433f2e7fb9d921
Stephen Cunningham <stephencork19@gmail.com>
2020-03-25 01:47:42 +0000

    server side and DialogFlow connected v2

--- config/production.js
+++ config/production.js
@@ -6,5 +6,5 @@ module.exports = {
     dialogFlowSessionID: process.env.DF_SESSION_ID,
     dialogFlowSessionLanguageCode: process.env.DF_SESSION_LANG_CODE,
     googleClientEmail: process.env.GOOGLE_CLIENT_EMAIL,
-    googlePrivateKey: process.env.GOOGLE_PRIVATE_EMAIL
+    googlePrivateKey: process.env.GOOGLE_PRIVATE_KEY
 }
