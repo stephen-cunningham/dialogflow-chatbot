12b88192f5238cbdaee59d52b3ed0756e0f9b216
Stephen Cunningham <stephencork19@gmail.com>
2020-03-25 16:52:58 +0000

    server side and dialogflow connected v7

--- gcpSetup.js
+++ gcpSetup.js
@@ -1,2 +1,2 @@
 const fs=require('fs');
-fs.writeFile(process.env.GCP_KEY_FILE, process.env.GCP_CRED, (err) => {});
+fs.writeFile(process.env.GCP_KEY_FILE, process.env.GCP_CREDENTIALS, (err) => {});
