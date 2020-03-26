bf76d2ae32371fe0b936cbd015481bbd77a4b062
Stephen Cunningham <stephencork19@gmail.com>
2020-03-26 01:10:07 +0000

    testing auth #2

--- config/keys.js
+++ config/keys.js
@@ -1,6 +1,6 @@
 //this ensures that the right set of keys are loaded based on the NODE environment variable
-if(process.env.NODE_ENV === 'production'){
-    module.exports = require('./production');
-}else{
+// if(process.env.NODE_ENV === 'production'){
+//     module.exports = require('./production');
+// }else{
     module.exports = require('./develop');
-}
+// }
