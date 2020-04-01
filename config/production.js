308ba68faccb3d27e99d459f850d0dcaead32ab6
Stephen Cunningham <stephencork19@gmail.com>
2020-04-01 12:30:15 +0100

    final auth test complete 2.0

--- config/production.js
+++ config/production.js
@@ -5,7 +5,7 @@ module.exports = {
     //these values are set in the Heroku config vars in order to protect the actual values for security reasons
     googleProjectID: process.env.GOOGLE_PROJECT_ID,
     dialogFlowSessionID: process.env.DIALOGFLOW_SESSION_ID,
-    dialogFlowSessionLanguageCode: process.env.DIALOGFLOW_LANGAUGE_CODE,
+    dialogFlowSessionLanguageCode: process.env.DIALOGFLOW_LANGUAGE_CODE,
     googleClientEmail: process.env.GOOGLE_CLIENT_EMAIL,
     googlePrivateKey: process.env.GOOGLE_PRIVATE_KEY.replace(/\\n/gm, '\n')
 };
