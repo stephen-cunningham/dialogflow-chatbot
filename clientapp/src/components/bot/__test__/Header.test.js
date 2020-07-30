f43f550cea225193e9608dee3d17997668cdd3b6
stephen-cunningham <stephencork19@gmail.com>
2020-07-31 00:24:22 +0100

    fixing vulnerabilities

--- clientapp/src/components/bot/__test__/Header.test.js
+++
@@ -1,9 +0,0 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import Header from '../../Header';
-import {isTSAnyKeyword} from '@babel/types';
-
-it("renders without crashing",  ()=>{
-    const div = document.createElement("div");
-    ReactDOM.render(<Header></Header>, div);
-});
